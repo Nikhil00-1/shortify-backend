bba8f8299dd6850fd908919b4e4afe58ad3efbc5
Nikhil00-1 <nikhilwagh206@gmail.com>
2025-10-24 22:50:08 +0530

    Authorization JWT secret 2

--- Service/auth.js
+++ Service/auth.js
@@ -1,13 +1,13 @@
 const jwt=require("jsonwebtoken")
 require("dotenv").config();
-const secret="ShortIFY@***"
+
 
 const setUser=(user)=>{
     return jwt.sign(
         {
             _id:user._id,
             email:user.email,
-        },JWT_SECRET );
+        },JWT_SECRET);
 };
 
 const getUser=(token)=>{
